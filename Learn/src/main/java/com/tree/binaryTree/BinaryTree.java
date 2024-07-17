18377755537abc829f83c8c6b4211be86e44c0d8
tqw <tqw>
2024-07-17 16:21:39 +0800

    111

---
+++ Learn/src/main/java/com/tree/binaryTree/BinaryTree.java
@@ -0,0 +1,20 @@
+package com.tree.binaryTree;
+
+/**
+ * 二叉树类
+ */
+public class BinaryTree {
+
+    private TreeNode tree;
+
+    //插入数据
+    public void insert(int value){
+        TreeNode node = new TreeNode(value);
+        if (tree==null){
+
+        }else {
+
+        }
+    }
+
+}
---
+++ Learn/src/main/java/com/tree/binaryTree/TreeNode.java
@@ -0,0 +1,17 @@
+package com.tree.binaryTree;
+
+/**
+ * 节点类
+ */
+public class TreeNode {
+    //值
+    private int value;
+    //左节点
+    private TreeNode left;
+    //右节点
+    private TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
